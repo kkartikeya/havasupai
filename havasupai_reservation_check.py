28839db16af9ef87b0d3cb41a0f54103768c5d27
KK <kk@retailnext.net>
2018-05-01 11:15:27 -0700

    Added rest of the code

--- havasupai_reservation_check.py
+++ havasupai_reservation_check.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 from datetime import timedelta
 import configparser
+import uuid
 import json
 import requests
 import urllib.request, urllib.parse
@@ -11,6 +12,7 @@ import urllib.request, urllib.parse
 # https://api.havasupaireservations.com/v1/Reserves/calendar?ec=2018-09-30&np=2&sc=2018-07-01&token=be43a8f7-213a-4339-92d1-327286ab1bd2
 NO_OF_PEOPLE=2
 CONFIG_FILE_PATH='/Users/kk/work/personal/github.com/configuration/config.properties'
+BASE_URL='https://api.havasupaireservations.com/v1/Reserves/calendar?'
 
 def getfirstdateofthemonth():
 	return datetime.today().replace(day=1)
@@ -37,22 +39,39 @@ def sendSlackMessage(message):
 
 	urllib.request.urlopen(slackurl, payload)
 
-def getSupaiReservationAvailibility(startdate, enddate):
-	sendSlackMessage('%s - %s' % (startdate, enddate))
+def getSupaiReservationAvailibility(startdate, enddate, token):
+	fullURL=BASE_URL+'ec=%s&np=%s&sc=%s&token=%s' %(enddate, NO_OF_PEOPLE, startdate, token)
+	httprequest=urllib.request.Request(fullURL)
 
+	try:
+		response=urllib.request.urlopen(httprequest)
+		availabilityJson=response.read().decode('utf-8')
+		parseAvailabilityJson(availabilityJson)
+	except urllib2.HTTPError as err:
+		sendSlackMessage('Error Code: %s, Error Msg: %s' % (err.code, err.msg) ) 
+
+def parseAvailabilityJson(availabilityJson):
+	availabilityDict=json.loads(availabilityJson)
+	for epochTime in availabilityDict.keys():
+		available=availabilityDict.get(epochTime)
+		availabilityDate=datetime.utcfromtimestamp(int(epochTime)).strftime("%Y-%m-%d")
+
+		if available=='true':
+			sendSlackMessage('%s - %s' % (availabilityDate, available))
 
 
 def main():
+	token=str(uuid.uuid4())
 	first_date=getfirstdateofthemonth()
 	last_date=getlastdateofthemonth(first_date)
 	month=first_date.month
 
 	while month <= 12:
-		getSupaiReservationAvailibility(first_date, last_date)
+		getSupaiReservationAvailibility(first_date.strftime("%Y-%m-%d"), last_date.strftime("%Y-%m-%d"), token)
 
 		if month==12:
 			break
-			
+
 		month+=1
 		first_date=first_date.replace(month=month)
 		last_date=getlastdateofthemonth(first_date)	
